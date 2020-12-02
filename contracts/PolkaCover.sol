a004725331c47458f3bdf831a2012dd06fcba112
“coderasha” <ashadipta@gmail.com>
2020-12-02 14:00:00 +0100

    Testing a GIt Repo

---
+++ contracts/PolkaCover.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+
+
+
+
+
+contract PolkaCover is ERC20 , ERC20Detailed , ERC20Burnable , ERC20Pausable {
+  constructor (uint256 _totalSupply)
+    
+    ERC20Detailed("PolkaCover" , "CVR")
+
+        public
+        {
+           
+           _mint(msg.sender, _totalSupply);
+          
+        }
+      
+}
